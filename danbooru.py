601019e8e0c299c06847a89d9a71517c9cafa68d
codestation <codestation404@gmail.com>
2012-02-08 11:08:20 -0430

    Added sample config, added readme and license.

--- danbooru.py
+++ danbooru.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+"""
+   Copyright 2012 codestation
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
 import os
 import sys
 import signal
+from genericpath import isfile
 from danbooru.api import Api
 from danbooru.database import Database
 from danbooru.settings import Settings
 from danbooru.downloader import Downloader
-from genericpath import isfile
-from danbooru.nepomuk import NepomukBus
 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
@@ -81,7 +96,8 @@ if __name__ == '__main__':
             dl.downloadQueue(rows, False)
             offset += 100
             
-    elif sys.argv[3] == 'nepomuk':
+    elif sys.argv[3] == 'nepomuk':        
+        from danbooru.nepomuk import NepomukBus
         nk = NepomukBus()
         for name in os.listdir(cfg.download_path):
             full = os.path.join(cfg.download_path, name)
--- danbooru/nepomuk.py
+++ danbooru/nepomuk.py
@@ -1,6 +1,24 @@
-from PyQt4 import QtCore
-from PyQt4.QtCore import QUrl
-from PyKDE4 import kdecore
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+   Copyright 2012 codestation
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+from os.path import abspath
+from PyKDE4.kdecore import KUrl
 from PyKDE4.nepomuk import Nepomuk
 
 class NepomukBus(object):
@@ -13,16 +31,15 @@ class NepomukBus(object):
         
     def removeProperties(self, res, ontologies):
         for ontology in ontologies:
-            res.removeProperty(QUrl(self.ndbu_uri % ontology))
+            res.removeProperty(KUrl(self.ndbu_uri % ontology))
             
     def setProperty(self, res, ontology, prop):
-        res.setProperty(QUrl(self.ndbu_uri % ontology), Nepomuk.Variant(prop))
+        res.setProperty(KUrl(self.ndbu_uri % ontology), Nepomuk.Variant(prop))
         
     def getResource(self, res):
         if isinstance(res, str):
-            file_info = QtCore.QFileInfo(res)
-            absolute_path = file_info.absoluteFilePath()
-            return Nepomuk.Resource(kdecore.KUrl(absolute_path))
+            absolute_path = abspath(res)
+            return Nepomuk.Resource(KUrl(absolute_path))
         else:
             return res
     
@@ -34,7 +51,7 @@ class NepomukBus(object):
             tag.setLabel(name)
             res.addTag(tag)
         if post['source']:     
-            self.setProperty(res, 'source', QUrl(post['source']))            
+            self.setProperty(res, 'source', KUrl(post['source']))            
         else:
             self.removeProperties(res, ['source'])
         self.setProperty(res, 'score', post['score'])
