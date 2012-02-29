824f6ee5b60f4c61dc4bd88c7f6178ebd7be8f7a
codestation <codestation404@gmail.com>
2012-02-28 20:36:38 -0430

    Moved functions and classes to danbooru/utils

--- danbooru/utils.py
+++ danbooru/utils.py
@@ -15,8 +15,46 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from os.path import join, basename, splitext
+from os import makedirs
 from urllib.parse import urlsplit
+from os.path import join, basename, splitext, exists
+from PyQt4 import QtCore, QtGui
+
+
+class ThumbnailCache(object):
+
+    MAX_SIZE = 256
+
+    def __init__(self, path):
+        self.path = path
+        makedirs(path, exist_ok=True)
+
+    def getThumbnail(self, path):
+        if exists(path):
+            base = splitext(basename(path))[0]
+            image = QtGui.QImage()
+            if image.load(join(self.path, base + ".png"), format="png"):
+                return image
+            else:
+                image = self.scaleImage(path, self.MAX_SIZE)
+                image.save(join(self.path, base + ".png"), format="png")
+                return image
+
+    def scaleImage(self, path, length):
+        image_reader = QtGui.QImageReader(path)
+        image_width = image_reader.size().width()
+        image_height = image_reader.size().height()
+        if image_width > image_height:
+            image_height = int(length * 1.0 / image_width * image_height)
+            image_width = length
+        elif image_width < image_height:
+            image_width = int(length * 1.0 / image_height * image_width)
+            image_height = length
+        else:
+            image_width = length
+            image_height = length
+        image_reader.setScaledSize(QtCore.QSize(image_width, image_height))
+        return QtGui.QImage(image_reader.read())
 
 
 def list_generator(list_widget):
@@ -37,3 +75,37 @@ def post_basename(post):
         return post['md5'] + ".jpg"
     else:
         return post['md5'] + splitext(base)[1]
+
+def parseDimension(term, dim):
+    query = {}
+    if term[len("%s:" % dim)] == ">":
+        query['%s_type' % dim] = ">"
+        query[dim] = int(term.split(">")[1])
+    elif term[len("%s:" % dim)] == "<":
+        query['%s_type' % dim] = "<"
+        query[dim] = int(term.split("<")[1])
+    else:
+        query['%s_type' % dim] = "="
+        query[dim] = int(term.split(":")[1])
+    return query
+
+
+def parseQuery(text):
+    query = {}
+    query['tags'] = []
+    items = re.sub(' +', ' ', text).split(' ')
+    try:
+        for item in items:
+            if item.startswith("site:"):
+                query['site'] = item.split(":")[1]
+            elif item.startswith("rating:"):
+                query['rating'] = item.split(":")[1]
+            elif item.startswith("width:"):
+                query.update(parseDimension(item, "width"))
+            elif item.startswith("height:"):
+                query.update(parseDimension(item, "height"))
+            else:
+                query['tags'].append(item)
+        return query
+    except Exception:
+        return item
--- danbooru_gui.py
+++ danbooru_gui.py
@@ -4,13 +4,10 @@
 
 import sys
 from os.path import join, dirname
-from PyQt4 import QtGui, uic
-from PyQt4.QtCore import QSize, Qt, QThread, pyqtSignal
+from PyQt4 import QtCore, QtGui, uic
 
-import query_parser
-from thumbnail import ThumbnailCache
+from danbooru import utils
 from danbooru.database import Database
-from danbooru.utils import list_generator, post_abspath, post_basename
 
 
 class DanbooruGUI(QtGui.QMainWindow):
@@ -66,7 +63,7 @@ class DanbooruGUI(QtGui.QMainWindow):
         self.clearButton = QtGui.QPushButton(self.queryBox)
         self.clearButton.setVisible(False)
         self.clearButton.setStyleSheet("QPushButton { border: none; padding: 0px; }")
-        self.clearButton.setCursor(QtGui.QCursor(Qt.ArrowCursor))
+        self.clearButton.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
         self.clearButton.setIcon(QtGui.QIcon("edit-clear-locationbar-rtl.png"))
         self.clearButton.clicked.connect(self.queryBox.clear)
         self.queryBox.textChanged.connect(self.updateClearButton)
@@ -98,9 +95,9 @@ class DanbooruGUI(QtGui.QMainWindow):
             width = size.width()
         size = image.size()
         if size.width() < size.height():
-            img = image.scaledToHeight(width, Qt.SmoothTransformation)
+            img = image.scaledToHeight(width, QtCore.Qt.SmoothTransformation)
         else:
-            img = image.scaledToWidth(width, Qt.SmoothTransformation)
+            img = image.scaledToWidth(width, QtCore.Qt.SmoothTransformation)
         return QtGui.QPixmap.fromImage(img)
 
     def selectionChanged(self):
@@ -111,8 +108,8 @@ class DanbooruGUI(QtGui.QMainWindow):
         elif len(items) == 1:
             item = items[0]
             self.nameLabel.setText("1 selected item")
-            post = item.data(Qt.UserRole)
-            full_path = post_abspath(post)
+            post = item.data(QtCore.Qt.UserRole)
+            full_path = utils.post_abspath(post)
             self.img = QtGui.QImage(full_path)
             self.updatePreview()
             tags = ["<a href='%s'>%s</a>" % (tag, tag) for tag in post['tags']]
@@ -130,8 +127,8 @@ class DanbooruGUI(QtGui.QMainWindow):
         self.startSearch()
 
     def itemClicked(self, item):
-        post = item.data(Qt.UserRole)
-        full_path = post_abspath(post)
+        post = item.data(QtCore.Qt.UserRole)
+        full_path = utils.post_abspath(post)
         self.previewWidget.setPixmap(QtGui.QPixmap(full_path))
 
     def makeIcon(self, item, image):
@@ -141,9 +138,9 @@ class DanbooruGUI(QtGui.QMainWindow):
         icon = QtGui.QIcon(pixmap)
         item.setIcon(icon)
 
-    class ThumbnailWorker(QThread):
+    class ThumbnailWorker(QtCore.QThread):
 
-        makeIconSignal = pyqtSignal(QtGui.QListWidgetItem, QtGui.QImage)
+        makeIconSignal = QtCore.pyqtSignal(QtGui.QListWidgetItem, QtGui.QImage)
         abort = False
 
         def __init__(self, ListWidget, parent=None):
@@ -156,37 +153,37 @@ class DanbooruGUI(QtGui.QMainWindow):
         def run(self):
             self.abort = False
             thumbnail_dir = join(dirname(sys.argv[0]), ".danbooru-thumbnails")
-            th = ThumbnailCache(thumbnail_dir)
-            generator = list_generator(self.widget)
+            th = utils.ThumbnailCache(thumbnail_dir)
+            generator = utils.list_generator(self.widget)
             for item in generator:
                 if self.abort: break
-                post = item.data(Qt.UserRole)
-                full_path = post_abspath(post)
+                post = item.data(QtCore.Qt.UserRole)
+                full_path = utils.post_abspath(post)
                 image = th.getThumbnail(full_path)
                 self.makeIconSignal.emit(item, image)
 
     def sliderMove(self, value):
         value *= self.SLIDER_MULT
         self.zoomSlider.setToolTip("Size: %i pixels" % value)
-        self.listWidget.setIconSize(QSize(value, value))
-        generator = list_generator(self.listWidget)
+        self.listWidget.setIconSize(QtCore.QSize(value, value))
+        generator = utils.list_generator(self.listWidget)
         for item in generator:
-            item.setSizeHint(QSize(value + 32,value + 32))
+            item.setSizeHint(QtCore.QSize(value + 32,value + 32))
         
     def addItem(self, post):        
         item = QtGui.QListWidgetItem()
-        item.setText(post_basename(post))
+        item.setText(utils.post_basename(post))
         item.setIcon(QtGui.QIcon().fromTheme("image-x-generic"))
-        item.setData(Qt.UserRole, post)
-        item.setTextAlignment(Qt.AlignHCenter | Qt.AlignBottom)
+        item.setData(QtCore.Qt.UserRole, post)
+        item.setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignBottom)
         value = self.zoomSlider.value() * self.SLIDER_MULT
-        item.setSizeHint(QSize(value + 32, value + 32))
+        item.setSizeHint(QtCore.QSize(value + 32, value + 32))
         self.listWidget.addItem(item)
 
     def startSearch(self):        
         text = self.queryBox.text().strip()
         if text:            
-            query = query_parser.parseQuery(text)
+            query = utils.parseQuery(text)
             if isinstance(query, str):
                 self.statusLabel.setText("Error in term: %s" % query)
                 return
@@ -210,7 +207,7 @@ class DanbooruGUI(QtGui.QMainWindow):
                 self.addItem(post)
             self.statusLabel.setText("Found %i images" % len(posts))
             size = self.zoomSlider.value() * self.SLIDER_MULT
-            self.listWidget.setIconSize(QSize(size, size))            
+            self.listWidget.setIconSize(QtCore.QSize(size, size))            
             self.thumb.start()
     
 if __name__ == '__main__':
